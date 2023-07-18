261f3783c9f04a9ed2ad6c2c10d9a6080daff5b0
MykolaIvanov1 <l0_t0s@ukr.net>
2023-07-18 10:29:02 +0300

    add hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import { Statistics } from './Feedback/Feedback';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+import css from './Feedback/Feedback.module.css';
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleFeedback = option => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [option]: prevFeedback[option] + 1,
+    }));
+  };
+
+  useEffect(() => {
+    console.log('Компонент App смонтирован.');
+
+    return () => {
+      console.log('Компонент App размонтирован.');
+    };
+  }, []); 
+
+  useEffect(() => {
+    console.log('Значение feedback изменилось:', feedback);
+  }, [feedback]); 
+
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = feedback;
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = feedback;
+    const total = countTotalFeedback();
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
+
+  const totalFeedback = countTotalFeedback();
+  const positiveFeedbackPercentage = countPositiveFeedbackPercentage();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.div}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+
+      {totalFeedback > 0 ? (
+        <Section title="Statistics">
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={totalFeedback}
+            positivePercentage={positiveFeedbackPercentage}
+          />
+        </Section>
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+      {totalFeedback > 0 && (
+        <Notification message="Thank you for your feedback!" />
+      )}
     </div>
   );
 };
+
+export default App;
